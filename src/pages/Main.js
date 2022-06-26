613dc373bb7761a3a290d555c72a7f4305d3ecd4
toku0223 <kei.nakanotoku@gmail.com>
2022-06-26 10:53:47 +0900

    main

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { Button } from '@material-ui/core';
 import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 
 
 const Main = () => {
     const Navigate = useNavigate()
+    useEffect(() => {
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/firebase.User
+                const uid = user.uid;
+                console.log(user)
+                // ...
+            } else {
+                // User is signed out
+                // ...
+                console.log(user)
+                Navigate('/Login')
+            }
+        });
+    }, [])
     const handleClick = () => {
         Navigate('/')
     };
